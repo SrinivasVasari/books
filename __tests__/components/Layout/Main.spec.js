2f6c7e9ac6aebd5da01c5af6f26dd5246912c2b5
SrinivasV <thanmay4@gmail.com>
2020-06-05 17:24:46 +0530

    test cases fixed for Main page Edit Book component render.

--- __tests__/components/Layout/Main.spec.js
+++ __tests__/components/Layout/Main.spec.js
@@ -2,6 +2,7 @@ import React from "react";
 import { shallow, mount } from "enzyme";
 import MainPage from "../../../components/Layout/Main";
 import BooksContext from "../../../context/context";
+import booksReducer from "../../../reducer/reducer";
 import EditBook from "../../../components/EditBook";
 
 describe("Main Component", () => {
@@ -13,5 +14,53 @@ describe("Main Component", () => {
     const spy = shallow(<MainPage />);
     const wrapper = spy.instance();
     expect(wrapper).toBeNull();
-  });  
+  });
+  it("Should returns the state of current Book", () => {
+    const state = {
+      currentBook:  {
+        books: {
+          bookAuthor: "test1",
+          bookDesc: "test2",
+          bookName: "test3",
+        },
+        id: "1",
+      },
+      books: [
+        {
+          books: {
+            bookAuthor: "test1",
+            bookDesc: "test2",
+            bookName: "test3",
+          },
+          id: "1",
+        },
+      ],
+    };
+    const currentBook = {
+      books: [
+        {
+          books: {
+            bookAuthor: "test1",
+            bookDesc: "test2",
+            bookName: "test3",
+          },
+          id: "1",
+        },
+      ],
+    };
+    const reducer = booksReducer(state, {
+      type: "SET_CURRENT_BOOK",
+      payload: {
+        id: "1",
+        books: { bookAuthor: "test1", bookDesc: "test2", bookName: "test3" },
+      },
+    });
+    const wrapper = mount(
+      <BooksContext.Provider value={ state }>
+        <MainPage {...currentBook} />
+      </BooksContext.Provider>
+    );
+
+    expect(reducer).not.toBeNull();
+  });
 });
