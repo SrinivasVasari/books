4b97fbd84abe1c499489b1b8cff5c62becb965da
SrinivasV <thanmay4@gmail.com>
2020-06-09 19:00:01 +0530

    changes done in main page test cases unnecessary lines removed.

--- __tests__/components/Layout/Main.spec.js
+++ __tests__/components/Layout/Main.spec.js
@@ -2,8 +2,6 @@ import React from "react";
 import { shallow, mount } from "enzyme";
 import MainPage from "../../../components/Layout/Main";
 import BooksContext from "../../../context/context";
-import booksReducer from "../../../reducer/reducer";
-import EditBook from "../../../components/EditBook";
 
 describe("Main Component", () => {
   it("should render correctly", () => {
@@ -36,31 +34,12 @@ describe("Main Component", () => {
         },
       ],
     };
-    const currentBook = {
-      books: [
-        {
-          books: {
-            bookAuthor: "test1",
-            bookDesc: "test2",
-            bookName: "test3",
-          },
-          id: "1",
-        },
-      ],
-    };
-    const reducer = booksReducer(state, {
-      type: "SET_CURRENT_BOOK",
-      payload: {
-        id: "1",
-        books: { bookAuthor: "test1", bookDesc: "test2", bookName: "test3" },
-      },
-    });
     const wrapper = mount(
       <BooksContext.Provider value={ state }>
-        <MainPage {...currentBook} />
+        <MainPage {...state.currentBook} />
       </BooksContext.Provider>
     );
 
-    expect(reducer).not.toBeNull();
+    expect(wrapper).not.toBeNull();
   });
 });
